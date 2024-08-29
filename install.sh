26ee64c12542f1e178df183fcc00b879424809b7
khanh vong <vongk7@gmail.com>
2024-08-28 22:55:28 -0500

    nvim added

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ rm -rf ~/.config/dunst
 rm -rf ~/scripts
 rm -rf ~/.config/qtile
 rm -rf ~/.config/rofi
+rm -rf ~/.config/nvim
 
 dir=$(pwd)
 
@@ -19,6 +20,7 @@ ln -s "${HOME}/dotfiles/dunst" ~/.config/dunst
 ln -s "${HOME}/dotfiles/scripts" ~/scripts
 ln -s "${HOME}/dotfiles/qtile" ~/.config/qtile
 ln -s "${HOME}/dotfiles/rofi" ~/.config/rofi
+ln -s "${HOME}/dotfiles/nvim" ~/.config/nvim
 
 source ~/.bashrc
 
@@ -28,7 +30,7 @@ sudo apt-get upgrade -y
 
 # Install base apps
 echo Installing base apps
-sudo apt-get -y install neovim htop keychain fzf vim rofi picom lxappearance vim thunar terminator dunst neofetch vifm conky-all rsync xclip
+sudo apt-get -y install neovim htop keychain fzf vim rofi picom lxappearance vim thunar terminator dunst neofetch vifm conky-all rsync xclip fd-find ripgrep 
 
 echo "Installation Completed"
 
