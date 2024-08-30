413f7558ea1a5eb4d43f96a8e2e7b1c4283bb9e5
khanh vong <vongk7@gmail.com>
2024-08-30 09:27:36 -0500

    updated nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -59,13 +59,23 @@ require("lazy").setup({
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     { 'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    { "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+          "MunifTanjim/nui.nvim",
+          -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
     }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
 
 vim.cmd.colorscheme "catppuccin"
@@ -75,3 +85,14 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+    ensure_installed = { "c", "lua", "bash", "python", "javascript", "html", "php" },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true }, 
+})
+
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
+vim.keymap.set("n", "<leader>bf", ":Neotree buffers toggle float<CR>", {})
