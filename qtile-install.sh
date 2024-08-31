771c1043fea36da0cd81c3cac213b96fd838e27f
khanh vong <vongk7@gmail.com>
2024-08-30 20:18:53 -0500

    default wallpaper added

--- qtile-install.sh
+++ qtile-install.sh
@@ -24,3 +24,20 @@ git clone https://github.com/dylanaraps/pywal
 cd pywal
 pip3 install --user .
 
+# Cache file for holding the current wallpaper
+cache_file="$HOME/.cache/current_wallpaper"
+rasi_file="$HOME/.cache/current_wallpaper.rasi"
+
+# Create cache file if not exists
+if [ ! -f $cache_file ] ;then
+    touch $cache_file
+    echo "$HOME/wallpaper/default.jpg" > "$cache_file"
+fi
+
+# Create rasi file if not exists
+if [ ! -f $rasi_file ] ;then
+    touch $rasi_file
+    echo "* { current-image: url(\"$HOME/wallpaper/default.jpg\", height); }" > "$rasi_file"
+fi
+
+
