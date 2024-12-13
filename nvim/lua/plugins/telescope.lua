af1b096d2289372f2ad0059b52ee57d692f1ba14
khanh vong <vongk7@gmail.com>
2024-12-12 21:52:43 -0600

    updated nvim telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,30 @@
 return { 
     'nvim-telescope/telescope.nvim', 
     tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' }
+    },
     config = function ()
+        require('telescope').setup{
+            pickers = {
+                find_files = {
+                    theme = "ivy"
+                },
+                buffers = {
+                    theme = "ivy"
+                },
+                live_grep = {
+                    theme = "ivy"
+                },
+            },
+            extensions = {
+                fzf = {}
+            }
+        }
+
+        require('telescope').load_extension('fzf')
+
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
