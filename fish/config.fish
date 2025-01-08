dc7158cd8b3068c976efa1e54cbb30872ac2a9e7
khanh vong <vongk7@gmail.com>
2025-01-07 22:59:26 -0600

    bring !! and !$ to fish

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,27 @@ source ~/.config/fish/conf.d/exports.fish
 source ~/.config/fish/conf.d/variables.fish
 source ~/.config/fish/conf.d/startup.fish
 enable_transience
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
