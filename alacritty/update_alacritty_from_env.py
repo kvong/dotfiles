88c2af7cb0c5e3c6a78b0bf0fb245c407497c1d7
khanh vong <vongk7@gmail.com>
2025-02-21 17:39:31 -0600

    fix update env script

--- alacritty/update_alacritty_from_env.py
+++ alacritty/update_alacritty_from_env.py
@@ -9,7 +9,7 @@ with open(config_path, "r") as f:
 
 # Update font family and size based on environment variables
 config["font"]["size"] = float(os.getenv("ALACRITTY_FONT_SIZE", 14))
-config["font.normal"]["family"] = os.getenv("ALACRITTY_FONT_FAMILY", 'FiraCode Nerd Font')
+config["font"]["normal"]["family"] = os.getenv("ALACRITTY_FONT_FAMILY", 'FiraCode Nerd Font')
 
 # Save the updated configuration
 with open(config_path, "w") as f:
